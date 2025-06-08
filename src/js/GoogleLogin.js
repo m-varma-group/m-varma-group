6485896ed4602a9bbcdbfbbb9a144f6df848e929
m-verma-group <mvermagroup@gmail.com>
2025-06-08 12:26:10 +0530

    redirectUri UPDATED

--- src/js/GoogleLogin.js
+++ src/js/GoogleLogin.js
@@ -32,7 +32,7 @@ export const useGoogleDriveLogin = ({
     flow: 'implicit',
 
     // Redirect URI used by Google after login completes
-    redirectUri: 'https://mak-group.vercel.app/oauth2/callback',
+    redirectUri: 'https://m-verma-group.vercel.app/oauth2/callback',
   });
 
   // Return the login function to be used in components
