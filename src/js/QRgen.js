62066659a897fffb71c2332af036449e7ced29b3
m-varma-group <mvarmagroup@gmail.com>
2026-03-01 17:03:25 +0530

    name, email, number optional

--- src/js/QRgen.js
+++ src/js/QRgen.js
@@ -25,6 +25,9 @@ const QRgen = ({ fileId, isFolder, fileName }) => {
   const [enableExpiry, setEnableExpiry] = useState(false);
   const [enablePassword, setEnablePassword] = useState(false);
   const [enableLabel, setEnableLabel] = useState(false);
+  const [requireName, setRequireName] = useState(false);
+  const [requireEmail, setRequireEmail] = useState(false);
+  const [requireMobile, setRequireMobile] = useState(false);
 
   const [noteContent, setNoteContent] = useState('');
 
@@ -254,6 +257,9 @@ const QRgen = ({ fileId, isFolder, fileName }) => {
       fileName,
       createdAt: serverTimestamp(),
     };
+    qrMetadata.requireName = requireName;
+    qrMetadata.requireEmail = requireEmail;
+    qrMetadata.requireMobile = requireMobile;
 
     if (enableNote) qrMetadata.message = noteContent;
     if (enableExpiry) qrMetadata.expiration = new Date(expiration);
@@ -392,6 +398,29 @@ const QRgen = ({ fileId, isFolder, fileName }) => {
                   checked={enableLabel}
                   onChange={() => setEnableLabel(!enableLabel)}
                 /> Set QR Label
+              </label>
+              <label>
+                <input
+                  type="checkbox"
+                  checked={requireName}
+                  onChange={() => setRequireName(!requireName)}
+                /> Require Visitor Name
+              </label>
+              
+              <label>
+                <input
+                  type="checkbox"
+                  checked={requireEmail}
+                  onChange={() => setRequireEmail(!requireEmail)}
+                /> Require Visitor Email
+              </label>
+              
+              <label>
+                <input
+                  type="checkbox"
+                  checked={requireMobile}
+                  onChange={() => setRequireMobile(!requireMobile)}
+                /> Require Visitor Mobile
               </label>
             </div>
 
